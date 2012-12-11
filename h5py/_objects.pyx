1270b6a5d4cfec0f245934477014b6533a27d89d
andrew.collette <andrew.collette@localhost>
2012-12-11 12:15:47 -0700

    More work on identifier management

--- h5py/_objects.pyx
+++ h5py/_objects.pyx
@@ -32,6 +32,126 @@ from defs cimport *
 
 from weakref import ref
 
+## {{{ http://code.activestate.com/recipes/577336/ (r3)
+from cpython cimport pythread
+from cpython.exc cimport PyErr_NoMemory
+
+cdef class FastRLock:
+    """Fast, re-entrant locking.
+
+    Under uncongested conditions, the lock is never acquired but only
+    counted.  Only when a second thread comes in and notices that the
+    lock is needed, it acquires the lock and notifies the first thread
+    to release it when it's done.  This is all made possible by the
+    wonderful GIL.
+    """
+    cdef pythread.PyThread_type_lock _real_lock
+    cdef long _owner            # ID of thread owning the lock
+    cdef int _count             # re-entry count
+    cdef int _pending_requests  # number of pending requests for real lock
+    cdef bint _is_locked        # whether the real lock is acquired
+
+    def __cinit__(self):
+        self._owner = -1
+        self._count = 0
+        self._is_locked = False
+        self._pending_requests = 0
+        self._real_lock = pythread.PyThread_allocate_lock()
+        if self._real_lock is NULL:
+            PyErr_NoMemory()
+
+    def __dealloc__(self):
+        if self._real_lock is not NULL:
+            pythread.PyThread_free_lock(self._real_lock)
+            self._real_lock = NULL
+
+    def acquire(self, bint blocking=True):
+        return lock_lock(self, pythread.PyThread_get_thread_ident(), blocking)
+
+    def release(self):
+        if self._owner != pythread.PyThread_get_thread_ident():
+            raise RuntimeError("cannot release un-acquired lock")
+        unlock_lock(self)
+
+    # compatibility with threading.RLock
+
+    def __enter__(self):
+        # self.acquire()
+        return lock_lock(self, pythread.PyThread_get_thread_ident(), True)
+
+    def __exit__(self, t, v, tb):
+        # self.release()
+        if self._owner != pythread.PyThread_get_thread_ident():
+            raise RuntimeError("cannot release un-acquired lock")
+        unlock_lock(self)
+
+    def _is_owned(self):
+        return self._owner == pythread.PyThread_get_thread_ident()
+
+
+cdef inline bint lock_lock(FastRLock lock, long current_thread, bint blocking) nogil:
+    # Note that this function *must* hold the GIL when being called.
+    # We just use 'nogil' in the signature to make sure that no Python
+    # code execution slips in that might free the GIL
+
+    if lock._count:
+        # locked! - by myself?
+        if current_thread == lock._owner:
+            lock._count += 1
+            return 1
+    elif not lock._pending_requests:
+        # not locked, not requested - go!
+        lock._owner = current_thread
+        lock._count = 1
+        return 1
+    # need to get the real lock
+    return _acquire_lock(
+        lock, current_thread,
+        pythread.WAIT_LOCK if blocking else pythread.NOWAIT_LOCK)
+
+cdef bint _acquire_lock(FastRLock lock, long current_thread, int wait) nogil:
+    # Note that this function *must* hold the GIL when being called.
+    # We just use 'nogil' in the signature to make sure that no Python
+    # code execution slips in that might free the GIL
+
+    if not lock._is_locked and not lock._pending_requests:
+        # someone owns it but didn't acquire the real lock - do that
+        # now and tell the owner to release it when done. Note that we
+        # do not release the GIL here as we must absolutely be the one
+        # who acquires the lock now.
+        if not pythread.PyThread_acquire_lock(lock._real_lock, wait):
+            return 0
+        #assert not lock._is_locked
+        lock._is_locked = True
+    lock._pending_requests += 1
+    with nogil:
+        # wait for the lock owning thread to release it
+        locked = pythread.PyThread_acquire_lock(lock._real_lock, wait)
+    lock._pending_requests -= 1
+    #assert not lock._is_locked
+    #assert lock._count == 0
+    if not locked:
+        return 0
+    lock._is_locked = True
+    lock._owner = current_thread
+    lock._count = 1
+    return 1
+
+cdef inline void unlock_lock(FastRLock lock) nogil:
+    # Note that this function *must* hold the GIL when being called.
+    # We just use 'nogil' in the signature to make sure that no Python
+    # code execution slips in that might free the GIL
+
+    #assert lock._owner == pythread.PyThread_get_thread_ident()
+    #assert lock._count > 0
+    lock._count -= 1
+    if lock._count == 0:
+        lock._owner = -1
+        if lock._is_locked:
+            pythread.PyThread_release_lock(lock._real_lock)
+            lock._is_locked = False
+## end of http://code.activestate.com/recipes/577336/ }}}
+
 # New strategy for identifier recycling management
 #
 # For objects like SpaceIDs and TypeIDs, close() operations only affect that
@@ -62,11 +182,11 @@ from weakref import ref
 #           lists
 #       * The attribute .locked is set to 0 (unlocked)
 #       * The attribute .nonlocal_close is set to 0 (local close only)
-#       * The attribute .manual_close is set to 0 (do not auto-close on __dealloc__)
+#       * The attribute .manual_close is set to 0 (auto-close on __dealloc__)
 #
 # 2. When ._close() is called:
-#       * If the identifier is locked
-#           - If it is invalid, RuntimeError is raised (this should never happen)
+#       * If the identifier is locked or already closed
+#           - Return without doing anything
 #       * The identifier is tested for validity
 #           - If the object is valid H5Idec_ref is called.
 #       * The attribute .id is set to 0
@@ -81,6 +201,8 @@ from weakref import ref
 #
 # Locking:
 #   * Whenever the parallel lists are accessed
+
+cdef FastRLock reglock = FastRLock()
 
 cdef list reg_refs = []
 cdef list reg_ids = []
@@ -98,30 +220,21 @@ cdef int reg_remove(obj) except -1:
     del reg_ids[idx]
     del reg_refs[idx]
 
-cdef int sweeping = 0
-
 cdef int reg_sweep() except -1:
     # Goes through the list and deletes all invalid identifiers by
     # manually close()-ing them
 
     cdef ObjectID obj
-    cdef list new_reg_refs = []
-    cdef list new_reg_ids = []
+    cdef list dead_objs
 
-    global sweeping, reg_refs, reg_ids
+    global reg_refs, reg_ids
 
-    if sweeping:
-        return 0
+    dead_objs = [r() for r in reg_refs if r() is not None and not r().valid]
+    for obj in dead_objs:
+        obj._c_close()
+    reg_refs = [r for r in reg_refs if r() is not None and r().valid]
+    reg_ids = [id(r()) for r in reg_refs]
 
-    sweeping = 1
-    try:
-        dead_objs = [r() for r in reg_refs if r() is not None and not r().valid]
-        for obj in dead_objs:
-            obj._c_close()
-        reg_refs = [r for r in reg_refs if r() is not None and r().valid]
-        reg_ids = [id(r()) for r in reg_refs]
-    finally:
-        sweeping = 0
 
 cdef class ObjectID:
 
@@ -143,16 +256,15 @@ cdef class ObjectID:
             return H5Iget_type(self.id) > 0
 
     def __cinit__(self, id):
+        global reglock
         self.id = id
         self.locked = 0
         self.nonlocal_close = 0
         self.manual_close = 0
-        reg_add(self)
+        with reglock:
+            reg_add(self)
 
     def _close(self):
-        self._c_close()
-
-    cdef int _c_close(self) except -1:
         """ Closes the object and wipes out this instance's hid_t.
 
         If the instance has .nonlocal_close nonzero, also sweeps the identifier
@@ -160,22 +272,30 @@ cdef class ObjectID:
 
         Does nothing in the case of a locked or already closed object
         """
-        if self.locked:
+        self._c_close()
+
+    cdef int _c_close(self) except -1:
+        # The actual logic is in a C method so it can be called in __dealloc__
+
+        global reglock
+        with reglock:
+
+            if self.locked:
+                return 0
+
+            if self.id == 0:    # Already closed
+                return 0
+
+            if self.valid:
+                H5Idec_ref(self.id)
+
+            self.id = 0
+
+            reg_remove(self)
+            if self.nonlocal_close:
+                reg_sweep()
+
             return 0
-
-        if self.id == 0:    # Already closed
-            return 0
-
-        if self.valid:
-            H5Idec_ref(self.id)
-
-        self.id = 0
-
-        reg_remove(self)
-        if self.nonlocal_close:
-            reg_sweep()
-
-        return 0
 
     def __dealloc__(self):
         if not self.manual_close:
